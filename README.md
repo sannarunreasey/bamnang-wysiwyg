e09a94f819b60cda25b7f9ea8b3f44e18685706c
sophearak <t.sophearak@gmail.com>
2014-01-25 09:34:08 +0700

    add editor new view

---
+++ README.md
@@ -0,0 +1,8 @@
+#Bamnang WYSIWYG
+
+##To get start
+
+- `git clone https://github.com/sophearak/bamnang-wysiwyg.git`
+- `cd bamnang-wysiwyg`
+- `npm install`
+- `node app.js`
---
+++ app.js
@@ -0,0 +1,113 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var app = express();
+var hbs = require('express-hbs');
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var viewsDir = __dirname + '/views';
+
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.static(__dirname + '/public'));
+
+// Hook in express-hbs and tell it where known directories reside
+app.engine('hbs', hbs.express3({
+  partialsDir: [__dirname + '/views/partials', __dirname + '/views/partials-other'],
+  layoutsDir: __dirname + '/views/layout',
+  defaultLayout: __dirname + '/views/layout/default.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+// Register sync helper
+hbs.registerHelper('link', function(text, options) {
+  var attrs = [];
+  for (var prop in options.hash) {
+    attrs.push(prop + '="' + options.hash[prop] + '"');
+  }
+  return new hbs.SafeString("<a " + attrs.join(" ") + ">" + text + "</a>");
+});
+
+// Register Async helpers
+hbs.registerAsyncHelper('readFile', function(filename, cb) {
+  fs.readFile(path.join(viewsDir, filename), 'utf8', function(err, content) {
+    cb(new hbs.SafeString(content));
+  });
+});
+
+var fruits = [
+  {name: 'apple'},
+  {name: 'orange'},
+  {name: 'pear'}
+];
+
+var veggies = [
+  {name: 'asparagus'},
+  {name: 'carrot'},
+  {name: 'spinach'}
+];
+
+app.get('/', function(req, res) {
+  res.render('index', {
+  title: 'express-hbs example'
+  });
+});
+
+app.get('/fruits', function(req, res) {
+  res.render('fruits/index-layoutsDir', {
+    title: 'My favorite fruits',
+    fruits: fruits
+  });
+});
+
+app.get('/fruits/:name', function(req, res) {
+  res.render('fruits/details-layoutsDir', {
+    fruit: req.params.name
+  })
+});
+
+app.get('/veggies', function(req, res) {
+  res.render('veggies', {
+    title: 'My favorite veggies',
+    veggies: veggies,
+    layout: 'veggie'
+  });
+});
+
+app.get('/veggies/explicit-dir', function(req, res) {
+  res.render('veggies', {
+    title: 'My favorite veggies',
+    veggies: veggies,
+    layout: __dirname + '/views/layout/veggie'
+  });
+});
+
+app.get('/veggies/:name', function(req, res) {
+  res.render('veggies/details', {
+    veggie: req.params.name,
+    layout: 'veggie-details'
+  })
+});
+
+
+
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
